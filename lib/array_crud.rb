d6dd28f6c9ef76ecf349fd039f8f13fca5aca2b7
Mauro Dorigo <mau.dorigo@outlook.es>
2020-08-20 18:42:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,7 +29,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   array = ["wow", "I", "am", "learning", "arrays!"]
-  array.index[2]
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
