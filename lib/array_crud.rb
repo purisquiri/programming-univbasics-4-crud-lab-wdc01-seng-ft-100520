fd3c7977979be978323fb25b71a2dd8c95813c8a
Mauro Dorigo <mau.dorigo@outlook.es>
2020-08-20 18:41:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,8 +28,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = ["I", "am", "learning", "arrays!"]
-  array.index[1]
+  array = ["wow", "I", "am", "learning", "arrays!"]
+  array.index[2]
 end
 
 def retrieve_first_element_from_array(array)
