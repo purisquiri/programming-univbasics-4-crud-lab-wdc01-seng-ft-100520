85e47e4b4525dc2534e2e988b1336c3ecfa6209c
Mauro Dorigo <mau.dorigo@outlook.es>
2020-08-20 18:35:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   array = ["HTML", "CSS", "JavaScript", "Ruby"]
-  array.unshift("wow!")
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
