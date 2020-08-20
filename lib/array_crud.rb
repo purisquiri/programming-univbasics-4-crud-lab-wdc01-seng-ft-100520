b346b1471b45b81aba34d0783c4d3a6aa2db5b54
Mauro Dorigo <mau.dorigo@outlook.es>
2020-08-20 18:29:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,6 +10,7 @@ def add_element_to_end_of_array(array, element)
   array = ["HTML", "CSS", "JavaScript", "Ruby"]
   element = "React"
   array.push(element)
+  array
 end
 
 def add_element_to_start_of_array(array, element)
